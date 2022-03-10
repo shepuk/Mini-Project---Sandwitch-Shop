2a4d760ad65a3288edc4a31a8ce4cef2ee581eee
shepuk <paulshepherd90@live.co.uk>
2022-03-10 18:32:32 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -63,14 +63,20 @@ def update_sales_worksheet(data):
     print("Sales worksheet updated successfully.\n")
 
 
-def calculate_surpluds_data(sales_data):
+def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for ech item type.
     """
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
 
 
 def main():
@@ -80,7 +86,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surpluds_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 
 print("Welcome to sandwich data automation.")
