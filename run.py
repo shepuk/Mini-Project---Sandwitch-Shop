e88c522d9c15eac140c905c2b8c725a406a26ec8
shepuk <paulshepherd90@live.co.uk>
2022-03-10 18:18:39 +0000

    Retrieve latest stock numbers from worksheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -62,6 +63,25 @@ def update_sales_worksheet(data):
     print("Sales worksheet updated successfully.\n")
 
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surpluds_data(sales_data):
+    """
+    Compare sales with stock and calculate the surplus for ech item type.
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surpluds_data(sales_data)
+
+
+print("Welcome to sandwich data automation.")
+main()
